949f028fa1afded78732d629e1ff439971a5956f
Kenny Levinsen <kl@kl.wtf>
2019-08-08 23:04:19 +0200

    build.rs: Avoid shelling out

--- build.rs
+++ build.rs
@@ -1,12 +1,20 @@
 use std::process::Command;
 
 fn main() {
-	Command::new("/bin/bash")
-                .arg("-c")
-                .arg("cd go; GO111MODULE=on go build -o libivy.a -buildmode=c-archive -ldflags='-s -w' libivy.go")
-                .output()
-                .unwrap();
-	println!("cargo:rustc-link-lib=static=ivy");
-	println!("cargo:rustc-link-search=native=./go/");
-	println!("cargo:rerun-if-changed=./go/");
+    let mut cmd = Command::new("go")
+        .arg("build")
+        .arg("-o")
+        .arg("libivy.a")
+        .arg("-buildmode=c-archive")
+        .arg("-ldflags=-s -w")
+        .arg("libivy.go")
+        .current_dir("go")
+        .env("GO111MODULE", "on")
+        .spawn()
+        .unwrap();
+
+    cmd.wait().unwrap();
+    println!("cargo:rustc-link-lib=static=ivy");
+    println!("cargo:rustc-link-search=native=./go/");
+    println!("cargo:rerun-if-changed=./go/");
 }
