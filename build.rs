feab908eb846fd72d2fb1e9b2bd1da67fbc29454
Kenny Levinsen <kl@kl.wtf>
2019-08-08 11:06:57 +0200

    Shrink library

--- build.rs
+++ build.rs
@@ -3,9 +3,10 @@ use std::process::Command;
 fn main() {
 	Command::new("/bin/bash")
                 .arg("-c")
-                .arg("cd go; GO111MODULE=on go build -o libivy.a -buildmode=c-archive libivy.go")
+                .arg("cd go; GO111MODULE=on go build -o libivy.a -buildmode=c-archive -ldflags='-s -w' libivy.go")
                 .output()
                 .unwrap();
 	println!("cargo:rustc-link-lib=static=ivy");
 	println!("cargo:rustc-link-search=native=./go/");
+	println!("cargo:rerun-if-changed=./go/");
 }
